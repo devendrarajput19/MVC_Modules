5c83abe11572d325845649ea6aba88fc02283384
Rajput <devendra.rajput.ext@nokia.com>
2021-09-16 13:34:19 +0530

    Consume WebAPI - HttpClient Get Method

---
+++ WEB API/WEBAPI_Project/GETMETHOD_with_HTTPCLIENT/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GETMETHOD_with_HTTPCLIENT
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Consume_WebApi().Wait();
+        }
+
+        static async Task Consume_WebApi()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:57489/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/employee/list");
+            if(response.IsSuccessStatusCode)
+            {
+                dynamic result = await response.Content.ReadAsStringAsync();
+                RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(result);
+
+                foreach(var item in rootObject.Employee)
+                {
+                    Console.WriteLine("{0}\t${1}\t{2}", item.EmployeeID, item.Email, item.Address, item.JoiningDate);
+                }
+
+                Console.ReadKey();
+            }
+
+        }
+
+        public class RootObject
+        {
+            public List<Employee> Employee { get; set; }
+        }
+
+        public class Employee
+        {
+            public int EmployeeID { get; set; }
+            public string Email { get; set; }
+            public string Address { get; set; }
+            public DateTime JoiningDate { get; set; }
+        }
+    }
+}
